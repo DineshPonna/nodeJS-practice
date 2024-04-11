5d6f2044f135303925b4a14335c52e7d04d9736f
DineshPonna <166280412+DineshPonna@users.noreply.github.com>
2024-04-12 00:29:15 +0530

    app.js

--- app.js
+++ app.js
@@ -59,15 +59,10 @@ app.post('/players/', async (request, response) => {
     INSERT INTO
       cricket_team(player_name,jersey_number,role)
     VALUES
-      (
-        '${playerName}',
-         ${jerseyNumber},
-         ${role}
-      );`
+      ('${playerName}',${jerseyNumber},'${role}');`
 
   const dbResponse = await db.run(addPlayerQuery)
-  const playerId = dbResponse.lastID
-  response.send({playerId: playerId})
+  response.send('Player Added to Team')
 })
 
 // GET player API
@@ -83,3 +78,37 @@ app.get('/players/:playerId/', async (request, response) => {
   const player = await db.get(getPlayerQuery)
   response.send(convertDbObjectToResponseObject(player))
 })
+
+//PUT player API
+app.put('/players/:playerId/', async (request, response) => {
+  const {playerId} = request.params
+  const playerDetails = request.body
+  const {playerName, jerseyNumber, role} = playerDetails
+  const updateplayerQuery = `
+    UPDATE
+      cricket_team 
+    SET
+      player_name='${playerName}',
+      jersey_number=${jerseyNumber},
+      role=${role} 
+    WHERE
+      player_id = ${playerId};`
+  await db.run(updateplayerQuery)
+  response.send('Player Details Updated')
+})
+
+//DELETE player
+app.delete('/players/:playerId/', async (request, response) => {
+  const {playerId} = request.params
+  const deletePlayerQuery = `
+    DELETE FROM
+      cricket_team 
+    WHERE
+      player_id = ${playerId};`
+  await db.run(deletePlayerQuery)
+  response.send('Player Removed')
+})
+
+//exporting
+module.exports = app
+
